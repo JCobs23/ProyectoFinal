cc6a6bd46fc9af247987602b74ab250885695cd1
lauthekiller <39975051+lauthekiller@users.noreply.github.com>
2025-05-21 15:01:03 -0500

    coso

---
+++ ProyectoFinal-JSL/Assets/Scripts/PinchoslLetales.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SpikeTrap : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("¡Te empalaste! Reiniciando escena... 🩸");
+
+            // Reinicia la escena actual
+            Scene currentScene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(currentScene.name);
+        }
+    }
+}
