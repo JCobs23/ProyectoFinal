7b613e0c432fdc25255768d00a274331c3840ce9
lauthekiller <39975051+lauthekiller@users.noreply.github.com>
2025-05-24 09:28:18 -0500

    peleando de nuevo con la llave 3.4

--- ProyectoFinal-JSL/Assets/Scripts/Recolectables/Llave.cs
+++ ProyectoFinal-JSL/Assets/Scripts/Recolectables/Llave.cs
@@ -1,30 +1,21 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-/// <summary>
-/// Clase <c>Llave</c> que permite cambiar de escena cuando el jugador la recoge.
-/// </summary>
 public class Llave : MonoBehaviour
 {
-    /// <summary>
-    /// Se ejecuta cuando un objeto colisiona con la llave. 
-    /// Si es el jugador, se carga la siguiente escena.
-    /// </summary>
-    /// <param name="collision">Colision detectada.</param>
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter(Collider other)
     {
-        if (collision.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
             int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
 
-            // Verifica si hay una siguiente escena antes de cargarla
             if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
             {
                 SceneManager.LoadScene(nextSceneIndex);
             }
             else
             {
-                Debug.Log("Ultima escena alcanzada.");
+                Debug.Log("Última escena alcanzada.");
             }
         }
     }
